006c61fcfb8304c4f7ea86e0792c282e8baba685
ZhokhovAlexey <BeaVe@ZhokhovAlexeys-MacBook-Pro.local>
2014-10-18 23:53:33 +0800

    Exclude httpclient and junit.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,11 +14,15 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        compile 'com.google.apis:google-api-services-drive:v2-rev123-1.18.0-rc'
+        compile 'com.google.apis:google-api-services-drive:v2-rev123-1.18.0-rc', {
+            excludes  'httpclient', 'junit'
+        }
         compile 'com.google.http-client:google-http-client-jackson2:1.18.0-rc', {
             excludes 'httpclient', 'httpcore', 'commons-logging'
         }
-        compile 'com.google.oauth-client:google-oauth-client-jetty:1.18.0-rc'
+        compile 'com.google.oauth-client:google-oauth-client-jetty:1.18.0-rc', {
+            excludes 'httpclient', 'junit'
+        }
 
         compile 'org.apache.tika:tika-core:1.5', {
             excludes 'xercesImpl', 'xmlParserAPIs', 'xml-apis', 'groovy'
